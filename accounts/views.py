6c478ae1c6856ffba026cfecf29acdd2f6e98bcf
tlsabadinas <treasure.abadinas@gmail.com>
2025-10-02 12:34:48 +0800

    feat(accounts): add login user interface and update template links

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def register(request):
     return render(request, 'accounts/register.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
