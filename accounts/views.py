d0227f843ec1679a045e1e765f6c7969eb88b604
Zander <zander.aligato25@gmail.com>
2025-10-18 00:58:05 +0800

    feat(accounts): added PUT and GET functionalities for api testing

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,12 @@ from django.contrib.auth import login as auth_login, authenticate, logout
 from django.contrib import messages
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
+#REST FRAMEWORKS
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
 from .models import User
 
 @csrf_protect
@@ -79,3 +85,25 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'You have been logged out successfully.')
     return redirect('login')
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_profile(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def edit_profile(request):
+    user = request.user
+    data = request.data
+
+    user.location = data.get('location', user.location)
+    user.genres = data.get('genres', user.genres)
+    user.instruments = data.get('instruments', user.instruments)
+    user.save()
+
+    serializer = UserSerializer(user)
+    return Response({'message': 'Profile updated successfully!', 'user': serializer.data})
